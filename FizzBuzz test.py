22f8831d9408d6ad38bdd370271dd464761a1974
James Zheng <95889499+JamesZheng109@users.noreply.github.com>
2022-02-09 16:00:02 -0500

    Update FizzBuzz test.py
    Added two new variables

--- FizzBuzz test.py
+++ FizzBuzz test.py
@@ -1,12 +1,12 @@
-def FizzBuzz(beg_num,end_num,num_1,num_2):
+def FizzBuzz(beg_num,end_num,num_1,num_2,word1,word2):
     for i in range(beg_num,end_num+1):
         t=i/num_1
         f=i/num_2
         if t.is_integer() and f.is_integer():
-            print('FizzBuzz')
+            print(f'{word1}{word2}')
         elif t.is_integer():
-            print('Fizz')
+            print(word1)
         elif f.is_integer():
-            print('Buzz')
+            print(word2)
         else:
             print(i)
