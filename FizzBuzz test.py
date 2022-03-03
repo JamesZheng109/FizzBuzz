9f28fe15e98a9032a8c417684044a7a9bdd1bb14
James Zheng <95889499+JamesZheng109@users.noreply.github.com>
2022-03-03 18:51:40 -0500

    Update FizzBuzz test.py
    Prints "Fizz" if word1 is left blank and "Buzz" if word2 is left blank

--- FizzBuzz test.py
+++ FizzBuzz test.py
@@ -1,12 +1,9 @@
-def FizzBuzz(beg_num,end_num,num_1,num_2,word1,word2):
+def FizzBuzz(beg_num,end_num,num_1,num_2,word1='',word2=''):
+    if word1=='':word1='Fizz'
+    if word2=='':word2='Buzz'
     for i in range(beg_num,end_num+1):
-        t=i/num_1
-        f=i/num_2
-        if t.is_integer() and f.is_integer():
-            print(f'{word1}{word2}')
-        elif t.is_integer():
-            print(word1)
-        elif f.is_integer():
-            print(word2)
-        else:
-            print(i)
+        t,f=i/num_1,i/num_2
+        if t.is_integer() and f.is_integer():print(f'{word1}{word2}')
+        elif t.is_integer():print(word1)
+        elif f.is_integer():print(word2)
+        else:print(i)
