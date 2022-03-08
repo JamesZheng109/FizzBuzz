7d3c4561e6d24b54d680a3585299535003208d2d
James Zheng <95889499+JamesZheng109@users.noreply.github.com>
2022-03-08 12:11:23 -0500

    Add default values for each variable

--- FizzBuzz test.py
+++ FizzBuzz test.py
@@ -1,6 +1,4 @@
-def FizzBuzz(beg_num,end_num,num_1,num_2,word1='',word2=''):
-    if word1=='':word1='Fizz'
-    if word2=='':word2='Buzz'
+def FizzBuzz(beg_num=0,end_num=30,num_1=3,num_2=5,word1='Fizz',word2='Buzz'):
     for i in range(beg_num,end_num+1):
         t,f=i/num_1,i/num_2
         if t.is_integer() and f.is_integer():print(f'{word1}{word2}')
